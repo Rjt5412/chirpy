cfe40ab7a6c5d0bc5f38bab99e08775e2a9e7285
Rjt5412 <rajat.malviya5412@gmail.com>
2023-09-18 12:06:20 +0530

    Added routing and related constraints

--- main.go
+++ main.go
@@ -1,8 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
+
+func hanlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
 
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -18,15 +50,21 @@ func middlewareCors(next http.Handler) http.Handler {
 }
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
-	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.Handle("/healthz", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(http.StatusText(http.StatusOK)))
-	}))
-	corsMux := middlewareCors(mux)
+
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
+
+	r := chi.NewRouter()
+	fsHandler := apiCfg.middewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	r.Handle("/app/*", fsHandler)
+	r.Handle("/app", fsHandler)
+	r.Get("/healthz", hanlerReadiness)
+	r.Get("/metrics", apiCfg.handlerMetrics)
+	r.HandleFunc("/reset", apiCfg.handlerReset)
+	corsMux := middlewareCors(r)
 	server := http.Server{
 		Addr:    ":" + port,
 		Handler: corsMux,
